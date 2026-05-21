e4d2bae95dbcbfda104726d9077cdd55007b1013
Damcza Dev <drma801@gmail.com>
2026-05-21 16:30:37 +0700

    feat: mount global account shortcut

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { AccountShortcut } from '@/components/account-shortcut';
 import { DlavieProviders } from '@/components/dlavie-providers';
 import '../styles/globals.css';
 
@@ -26,5 +27,5 @@ export default function App({ Component, pageProps }: AppProps) {
     };
   }, [router.events]);
 
-  return <DlavieProviders><DlavieLoader active={loading} /><div className="transition-opacity duration-300"><Component {...pageProps} /></div></DlavieProviders>;
+  return <DlavieProviders><DlavieLoader active={loading} /><AccountShortcut /><div className="transition-opacity duration-300"><Component {...pageProps} /></div></DlavieProviders>;
 }
