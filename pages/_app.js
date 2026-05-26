5bc46a07259e35aa2bff304fbdede35493d2cb25
Damcza Dev <drma801@gmail.com>
2026-05-26 10:49:03 +0700

    fix: add stable js app shell

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+import '../styles/ambient.css';
+import '../styles/cosmic.css';
+import '../styles/dlavie-system.css';
+import '../styles/dlavie-motion.css';
+import '../styles/auth-motion.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
